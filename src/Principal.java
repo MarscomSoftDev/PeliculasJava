e1b39aba34cb9881eac10df5dcba8d2c670c1a84
Jonathan Martinez <marscompereira@gmail.com>
2025-11-29 20:55:10 -0500

    ejercicio invirtinedo cadena

---
+++ src/Principal.java
@@ -0,0 +1,25 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        System.out.println("Mi pelicula es: " + miPelicula.nombre);
+        System.out.println("Fue lanzada el:" + miPelicula.fechaDeLanzamiento);
+        System.out.println("Con una duracion de: " + miPelicula.duracionEnMinutos);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "matrix";
+        otraPelicula.fechaDeLanzamiento = 1988;
+        otraPelicula.duracionEnMinutos = 180;
+
+        System.out.println("Mi pelicula es: " + otraPelicula.nombre);
+        System.out.println("Fue lanzada el:" + otraPelicula.fechaDeLanzamiento);
+        System.out.println("Con una duracion de: " + otraPelicula.duracionEnMinutos);
+
+
+
+    }
+}
