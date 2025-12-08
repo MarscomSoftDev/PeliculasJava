381758f2bf658e4e40e1d9ea2d82e93209fbe717
Jonathan Martinez <marscompereira@gmail.com>
2025-12-08 17:20:57 -0500

    encapsulamiento

---
+++ src/ejercicioLibro.java
@@ -0,0 +1,13 @@
+public class ejercicioLibro {
+
+   public class libro {
+       private String ISBN;
+       private String titulo;
+       private int numeroPaginas;
+       private String autor;
+       private Boolean Prestado;
+
+       public libro() {
+       }
+   }
+}
